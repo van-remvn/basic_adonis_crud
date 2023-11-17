13b4c282b7068e2e56bbb64c381bc97d596de57e
van.nguyen <nguyenngocvan279@gmail.com>
2023-11-17 08:51:35 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple CRUD with AdonisJS
+
+Basic CRUD using Node Js Framework
+
+## Getting Started
+
+```bash
+# clone to current directory
+git clone https://github.com/gempong/basic-crud-using-adonis.js.git
+
+# install dependencies
+npm install
+```
+
+Copy .env.example and rename to .env
+
+Edit the .env file to match your database settings.
+
+```bash
+# run your migrations
+adonis migration:run
+
+# Start the dev server
+adonis serve --dev
+```
---
+++ app/Controllers/Http/TodoController.js
@@ -0,0 +1,103 @@
+'use strict'
+const Todo = use('App/Models/Todo');
+const CloudinaryService = use('App/Services/CloudinaryService');
+const Helpers = use('Helpers')
+
+
+class TodoController {
+  async index({ request, response, view }) {
+    const todos = await Todo.all();
+    return view.render('index', { todos: todos.rows })
+  }
+
+  create({ request, response, view }) {
+    return view.render('create')
+  }
+
+  async store({ request, response, view, session }) {
+
+    const { title, description } = request.all();
+    const file = request.file('img');
+    try {
+      const todo = new Todo();
+
+      todo.title = request.input('title');
+      todo.description = request.input('description');
+
+      if (file !== null) {
+        const image = request.file('img', {
+          types: ['image'],
+          size: '2mb'
+        })
+        const filename = new Date().getTime() + '.' + image.subtype
+        await image.move(Helpers.publicPath('images'), {
+          name: `${filename}`,
+          overwrite: true
+        })
+        todo.img = `images/${filename}`
+      }
+
+      await todo.save();
+      session.flash({ notification: 'Successfully create!' });
+      return response.route('Todo.index')
+
+    } catch (error) {
+      session.flash({ notification: 'Error Uploading Image' });
+      return response.route('Todo.index')
+    }
+  }
+
+  async edit({ request, response, view, params }) {
+    const id = params.id;
+    const todo = await Todo.find(id);
+
+    return view.render('edit', { todo: todo })
+  }
+
+  async update({ request, response, view, params, session }) {
+
+    const { title, description } = request.all();
+    const file = request.file('img');
+    try {
+      const id = params.id;
+      const todo = await Todo.find(id);
+
+      todo.title = request.input('title');
+      todo.description = request.input('description');
+
+      if (file !== null) {
+        const image = request.file('img', {
+          types: ['image'],
+          size: '2mb'
+        })
+        const filename = new Date().getTime() + '.' + image.subtype
+        await image.move(Helpers.publicPath('images'), {
+          name: `${filename}`,
+          overwrite: true
+        })
+        todo.img = `images/${filename}`
+      }
+
+      await todo.save();
+      session.flash({ notification: 'Successfully Update!' });
+      return response.route('Todo.index')
+    } catch (error) {
+      session.flash({ notification: 'Error Uploading Image' });
+      return response.route('Todo.index')
+    }
+
+  }
+
+  async delete({ request, response, view, params, session }) {
+
+    const id = params.id;
+    const todo = await Todo.find(id);
+
+    await todo.delete();
+
+    session.flash({ notification: 'Successfully delete!' });
+    response.redirect('/')
+  }
+}
+
+module.exports = TodoController
